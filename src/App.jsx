f3a508408de73e00324d1c38d8ee1fb2d94df744
Abhishek Kumar <abhi7376862392@gmail.com>
2024-05-02 22:18:11 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./Home";
 import Description from "./Description";
 import Card1 from "./card1";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Description />
       {/* <Home /> */}
       <Card1 />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
     <div>
       {/* Navbar */}
       <nav class="navbar bg-body-tertiary fixed-top navbar-expand-lg">
-       
         <div class="container-fluid">
-            <div class="navbar-nav" >
-                <a class="nav-link" href="#"> Travel Agency</a>
+          <div class="navbar-nav">
+            <a class="nav-link" href="#">
+              Travel Agency
+            </a>
+          </div>
+          <div
+            class="collapse navbar-collapse nav justify-content-center"
+            id="navbarNavAltMarkup"
+          >
+            <div class="navbar-nav">
+              <a class="nav-link" href="#">
+                Home
+              </a>
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+              <a class="nav-link" href="#">
+                About us
+              </a>
+              <a class="nav-link" href="#">
+                Contact us
+              </a>
             </div>
-            <div class="collapse navbar-collapse nav justify-content-center" id="navbarNavAltMarkup">
-                <div class="navbar-nav" >
-                 <a class="nav-link" href="#">Home</a>
-                 <a class="nav-link" href="#">Pricing</a>
-                 <a class="nav-link" href="#">About us</a>
-                 <a class="nav-link" href="#">Contact us</a>
-                </div>
-               </div>
+          </div>
 
-               <button class="btn btn-danger  px-2 me-2 " >Sign up</button>
-             
-              
-           </div> 
-    </nav>
-      
+          <button class="btn btn-danger  px-2 me-2 ">Sign up</button>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
