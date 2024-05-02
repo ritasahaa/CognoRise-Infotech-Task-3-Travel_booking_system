c092f0c38e699d6a9d3cee376c40c176ae56e2d8
VivekAgastaya <hrpsva.vns@gmail.com>
2024-05-02 21:53:19 +0530

    added navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      {/* Navbar */}
+      <nav class="navbar bg-body-tertiary fixed-top navbar-expand-lg">
+       
+        <div class="container-fluid">
+            <div class="navbar-nav" >
+                <a class="nav-link" href="#"> Travel Agency</a>
+            </div>
+            <div class="collapse navbar-collapse nav justify-content-center" id="navbarNavAltMarkup">
+                <div class="navbar-nav" >
+                 <a class="nav-link" href="#">Home</a>
+                 <a class="nav-link" href="#">Pricing</a>
+                 <a class="nav-link" href="#">About us</a>
+                 <a class="nav-link" href="#">Contact us</a>
+                </div>
+               </div>
+
+               <button class="btn btn-danger  px-2 me-2 " >Sign up</button>
+             
+              
+           </div> 
+    </nav>
+      
+    </div>
   )
 }
 
