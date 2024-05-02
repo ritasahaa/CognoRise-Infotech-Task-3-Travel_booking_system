d3c65bcb3c5650f1849572d39434de9312915733
Abhishek Kumar <abhi7376862392@gmail.com>
2024-05-02 21:33:21 +0530

    caed1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Home from "./Home";
 import Description from "./Description";
+import Card1 from "./card1";
 
 function App() {
-
- 
   return (
     <>
-      <Description/>
-      <Home/>
+      <Description />
+      <Home />
+      <Card1 />
     </>
   );
 }
