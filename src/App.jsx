7af2e125bc37fc92e18505aa889fb42927ed95a2
ritasahaa <ritasah1602@gmail.com>
2024-05-03 12:36:01 +0530

    minor issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Home from "./Home";
 import Description from "./Description";
 import Card1 from "./card1";
 import Navbar from "./Navbar";
+import Cards from "./cards";
 
 function App() {
   return (
     <>
       <Navbar />
       <Description />
-      {/* <Home /> */}
+      <Home />
       <Card1 />
+      <Cards/>
     </>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,22 +3,22 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex justify-around bg-zinc-200 py-4  fixed w-full font-medium z-10">
-      <a class="nav-link" href="#">
+      <a className="nav-link" href="#">
         Travel Agency
       </a>
-      <a class="nav-link" href="#">
+      <a className="nav-link" href="#">
         Home
       </a>
-      <a class="nav-link" href="#">
+      <a className="nav-link" href="#">
         Pricing
       </a>
-      <a class="nav-link" href="">
+      <a className="nav-link" href="">
         About us
       </a>
-      <a class="nav-link" href="#">
+      <a className="nav-link" href="#">
         Contact us
       </a>
-      <button class="btn btn-danger  px-2 me-2 ">Sign up</button>
+      <button className="btn btn-danger  px-2 me-2 ">Sign up</button>
     </nav>
   );
 };
