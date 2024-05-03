3deb31f91a4ee2c204a125d6c3234f8d8de351ca
ritasahaa <ritasah1602@gmail.com>
2024-05-03 12:51:26 +0530

    fix error in card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import Home from "./Home";
 import Description from "./Description";
-import Card1 from "./card1";
+// import Card1 from "./card1";
 import Navbar from "./Navbar";
 import Cards from "./cards";
 
-<<<<<<< HEAD
 const App = () => {
 
-=======
-function App() {
->>>>>>> df499b836627f02a8418cadc30afc970fcb5cc44
   return (
     <>
       <Navbar />
       <Description />
       <Home />
-      <Card1 />
+      {/* <Card1 /> */}
       <Cards/>
     </>
   );
