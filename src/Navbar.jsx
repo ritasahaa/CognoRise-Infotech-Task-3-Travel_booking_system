feb6d3ecc59c542f98be2f69eabbae521f239893
VivekAgastaya <hrpsva.vns@gmail.com>
2024-05-04 12:20:45 +0530

    fix minor issue in navbar component

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,24 +2,28 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-around bg-zinc-200 py-4  fixed w-full font-medium z-10">
-      <a className="nav-link" href="#">
-        Travel Agency
-      </a>
-      <a className="nav-link" href="#">
-        Home
-      </a>
-      <a className="nav-link" href="#">
-        Pricing
-      </a>
-      <a className="nav-link" href="">
-        About us
-      </a>
-      <a className="nav-link" href="#">
-        Contact us
-      </a>
-      <button className="btn btn-danger  px-2 me-2 ">Sign up</button>
-    </nav>
+   <div>
+    <nav className="navbar bg-body-tertiary fixed-top navbar-expand-lg border-bottom ">
+      <div className="container-fluid">
+      <div className="navbar-nav ">
+        <a className="nav-link" href="#">Travel Agency</a>
+      </div>
+
+      <div className=" justify-content-center" id="navbarNavAltMarkup">
+      <div className="navbar-nav px-2 me-2"> 
+          <a className="nav-link" href="#">Home</a>
+          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link" href="#">About us</a>
+          <a className="nav-link" href="#">Contact us</a>
+      </div>
+      </div>
+      
+      <button class="btn btn-primary " >Sign up</button>
+      </div>
+       </nav>
+      
+    
+   </div>
   );
 };
 
