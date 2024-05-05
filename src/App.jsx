bfc38f772154990b5ab5b38f755c266ef92df195
VivekAgastaya <hrpsva.vns@gmail.com>
2024-05-05 15:35:47 +0530

    no bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 <<<<<<< HEAD
-// import Home from "./Home";
-// import Description from "./Description";
-// import Navbar from "./Navbar";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import RecentTrip from "./RecentTrip";
-// import Footer from "./Footer";
+import Home from "./Home";
+import Description from "./Description";
+import Navbar from "./Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import RecentTrip from "./RecentTrip";
+import Footer from "./Footer";
 import RouteConfig from "./Route";
 
 =======
-import RouteConfig from "./RouteConfig"
+import RouteConfig from "./RouteConfig";
 >>>>>>> e400e1861397b118b9e3d3aa90842bc8bbdc7257
 
 const App = () => {
